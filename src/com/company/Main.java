250ea2a773d260af7f91e195cf9045ab9074cac3
Yahor Makarchuk <egor.makarchuk00@gmail.com>
2020-05-28 00:32:44 +0300

    new commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
         MainFrame mainFrame = new MainFrame();
-        mainFrame.initialize();
+        mainFrame.initialize(); 
     }
 }
